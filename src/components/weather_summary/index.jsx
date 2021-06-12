ded35ac4cfeba1643f640564d0bd5c12d796ff93
Karin Stenwall <karin.stenwall@gmail.com>
2021-06-12 21:03:43 +0200

    add: 7 day forecast for current location

--- src/components/weather_summary/index.jsx
+++ src/components/weather_summary/index.jsx
@@ -1,46 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { FETCH } from '../../services/WeatherService';
+import ForecastDay from '../forecast_day';
+import ForecastWeek from '../forecast_week';
 import WeatherInfo from '../weather_info';
 import './WeatherSummary.scss';
 
 const WeatherSummary = ({ location }) => {
   const [weather, setWeather] = useState(null),
-        [forecast, setForecast] = useState(null);
+        [forecastDay, setForecastDay] = useState(null),
+        [forecastWeek, setForecastWeek] = useState(null);
 
   useEffect(() => {
     (async () => {
       if (location) {
         await Promise.all([
-          FETCH(`weather?id=${location.id}&units=metric`)
-            .then((data) => {
-              setWeather(data);
-            }),
-          FETCH(`forecast?id=${location.id}&units=metric&cnt=8`)
-            .then((data) => {
-              setForecast(data.list);
-            })
+          FETCH(`weather?id=${location.id}&units=metric`).then((data) => {
+            setWeather(data);
+          }),
+          FETCH(`forecast?id=${location.id}&units=metric&cnt=8`).then(
+            (data) => {
+              setForecastDay(data.list);
+            }
+          ),
+          FETCH(
+            `onecall?lat=${location.coord.lat}&lon=${location.coord.lon}&exclude=minutely,hourly&units=metric`
+          ).then((data) => {
+            setForecastWeek(data.daily);
+            console.log(data);
+            console.log(data.daily);
+          }),
         ]);
       }
     })();
   }, [location])
 
-  if (!location || !weather || !forecast) return null;
+  if (!location || !weather || !forecastDay || !forecastWeek) return null;
 
   return (
     <>
       <h2>{location.name}</h2>
       <WeatherInfo weather={weather} />
 
-      <h2>Forecast</h2>
+      <h2>24 hour forecast</h2>
       <div>
         <ol>
-          {forecast.map((timePoint) => (
-            <li key={timePoint.dt}>
-              <WeatherInfo weather={timePoint} />
+          {forecastDay.map((time) => (
+            <li key={time.dt}>
+              <ForecastDay weather={time} />
             </li>
           ))}
         </ol>
       </div>
+
+      <h2>7 day forecast</h2>
+      {forecastWeek.map((day) => (
+        <ForecastWeek weather={day} />
+      ))}
     </>
   );
 }
