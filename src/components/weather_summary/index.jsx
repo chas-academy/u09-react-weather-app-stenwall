809ce34d89bf2e2b6927e0d2879979a4d0334eb8
Karin Stenwall <karin.stenwall@gmail.com>
2021-06-12 21:40:09 +0200

    fix: clean up code in weather summary component

--- src/components/weather_summary/index.jsx
+++ src/components/weather_summary/index.jsx
@@ -8,54 +8,67 @@ import './WeatherSummary.scss';
 const WeatherSummary = ({ location }) => {
   const [weather, setWeather] = useState(null),
         [forecastDay, setForecastDay] = useState(null),
-        [forecastWeek, setForecastWeek] = useState(null);
+        [forecastWeek, setForecastWeek] = useState(null),
+        [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     (async () => {
       if (location) {
         await Promise.all([
-          FETCH(`weather?id=${location.id}&units=metric`).then((data) => {
-            setWeather(data);
-          }),
-          FETCH(`forecast?id=${location.id}&units=metric&cnt=8`).then(
-            (data) => {
+          FETCH(`weather?id=${location.id}&units=metric`)
+            .then((data) => {
+              setWeather(data);
+            }),
+          FETCH(`forecast?id=${location.id}&units=metric&cnt=8`)
+            .then((data) => {
               setForecastDay(data.list);
-            }
-          ),
-          FETCH(
-            `onecall?lat=${location.coord.lat}&lon=${location.coord.lon}&exclude=minutely,hourly&units=metric`
-          ).then((data) => {
-            setForecastWeek(data.daily);
-            console.log(data);
-            console.log(data.daily);
-          }),
-        ]);
+            }),
+          FETCH(`onecall?lat=${location.coord.lat}&lon=${location.coord.lon}&exclude=minutely,hourly&units=metric`)
+            .then((data) => {
+              setForecastWeek(data.daily);
+            }),
+        ])
+          .finally(() => {
+            setLoading(false);
+          });
       }
     })();
   }, [location])
 
-  if (!location || !weather || !forecastDay || !forecastWeek) return null;
+  // if (!location || !weather || !forecastDay || !forecastWeek) return null;
 
   return (
     <>
-      <h2>{location.name}</h2>
-      <WeatherInfo weather={weather} />
+      {!loading && location && (
+        <>
+          <h2>{location.name}</h2>
+          {weather && <WeatherInfo weather={weather} />}
 
-      <h2>24 hour forecast</h2>
-      <div>
-        <ol>
-          {forecastDay.map((time) => (
-            <li key={time.dt}>
-              <ForecastDay weather={time} />
-            </li>
-          ))}
-        </ol>
-      </div>
+          <h2>24 hour forecast</h2>
+          <ol>
+            {forecastDay &&
+              forecastDay.map((time) => (
+                <li key={time.dt}>
+                  <ForecastDay weather={time} />
+                </li>
+              ))}
+          </ol>
 
-      <h2>7 day forecast</h2>
-      {forecastWeek.map((day) => (
-        <ForecastWeek weather={day} />
-      ))}
+          <h2>7 day forecast</h2>
+          <ol>
+            {forecastWeek &&
+              forecastWeek.map((day) => (
+                <li key={day.dt}>
+                  <ForecastWeek weather={day} />
+                </li>
+              ))}
+          </ol>
+        </>
+      )}
+
+      {loading && <p>Loading...</p>}
     </>
   );
 }
