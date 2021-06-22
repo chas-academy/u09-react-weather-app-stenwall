b7bf8dfd63ebdd4d3691c67dfdb48e1787d457bd
Karin Stenwall <karin.stenwall@gmail.com>
2021-06-22 14:47:55 +0200

    wip: styling for forecast week

--- src/components/forecast_week/ForecastWeekDetails.jsx
+++ src/components/forecast_week/ForecastWeekDetails.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
 const unixTimeToDate = (unixUTC) => {
   return new Date(unixUTC * 1000);
@@ -19,52 +21,74 @@ const timeOptions = {
 const ForecastWeekDetails = ({ weather, time, temp, details, tod, units }) => {
   return (
     <div className="details-wrapper">
-      <div className="details-overview">
+
+      <div className="table-card details-overview">
         <span className="time">{unixTimeToDate(time).toLocaleDateString([], dateOptions)}</span>
         <span className="temp">{Math.round(temp.day)}° {units.deg}</span>
       </div>
-      <div className="weather">
-        <i className={`wi wi-owm-${tod}-${details.id}`}></i>
+
+      <div className="table-card weather">
+        <i className={`wi wi-owm-${tod}-${details.id} icon`}></i>
         <span>{details.main}</span>
         <span>{details.description}</span>
       </div>
-      <div className="humidity">
-        <i className={`wi wi-humidity`}></i>
+
+      <div className="table-card feels-like">
+        <i className={`wi wi-thermometer icon wi-fw`}></i>
+        <span>Feels like</span>
+        <span>{Math.round(weather.feels_like.day)}° {units.deg}</span>
+      </div>
+
+      <div className="table-card humidity">
+        <i className={`wi wi-humidity icon wi-fw`}></i>
         <span>Humidity</span>
         <span>{weather.humidity} %</span>
       </div>
-      <div className="rain">
-        <i className={`wi wi-umbrella`}></i>
-        <span>Rain</span>
-        <span>{Math.round(weather.rain) || '0'} mm</span>
-      </div>
-      <div className="wind">
-        <i className={`wi wi-wind from-${weather.wind_deg}-deg`}></i>
+
+      <div className="table-card wind">
+        <i className={`wi wi-wind from-${weather.wind_deg}-deg icon wi-fw`}></i>
+        <span>Wind (gust wind)</span>
         <span>
-          Wind (gust wind)
-        </span>
-        <span>
-          {Math.round(weather.wind_speed)} ({Math.round(weather.wind_gust)}){' '}
+          {Math.round(weather.wind_speed)} ({Math.round(weather.wind_gust)}) 
           {units.speed}
         </span>
       </div>
-      <div className="sunrise">
-        <i className={`wi wi-sunrise`}></i>
+
+      <div className="table-card rain">
+        <i className={`wi wi-umbrella icon wi-fw`}></i>
+        <span>Rain</span>
+        <span>{Math.round(weather.rain) || '0'} mm</span>
+      </div>
+
+      <div className="table-card uv-index">
+        <FontAwesomeIcon
+          className="icon"
+          icon={faSun}
+        />
+        <span>UV Index</span>
+        <span>{weather.uvi}</span>
+      </div>
+
+      <div className="table-card pressure">
+        <i className={`wi wi-barometer icon wi-fw`}></i>
+        <span>Pressure</span>
+        <span>{weather.pressure} hPa</span>
+      </div>
+
+      <div className="table-card sunrise">
+        <i className={`wi wi-sunrise icon wi-fw`}></i>
         <span>Sunrise</span>
         <span>{unixTimeToDate(weather.sunrise).toLocaleTimeString([], timeOptions)}
         </span>
       </div>
-      <div className="sunset">
-        <i className={`wi wi-sunset`}></i>
+
+      <div className="table-card sunset">
+        <i className={`wi wi-sunset icon wi-fw`}></i>
         <span>Sunset</span>
         <span>{unixTimeToDate(weather.sunset).toLocaleTimeString([], timeOptions)}
         </span>
       </div>
-      <div className="feels-like">
-        <i className={`wi wi-thermometer`}></i>
-        <span>Feels like</span>
-        <span>{Math.round(weather.feels_like.day)}° {units.deg}</span>
-      </div>
+
     </div>
   );
 };
