e2865ea2b4e5e337c4f184792004a094ee78564c
Karin Stenwall <karin.stenwall@gmail.com>
2021-06-30 22:34:36 +0200

    move: api calls from main component to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FETCH } from '../src/services/WeatherService';
+// import ApiCaller from './components/api_caller';
 import MessageError from './components/message_error';
 import MessageWarning from './components/message_warning';
 import Main from './components/main';
@@ -11,6 +12,10 @@ const App = () => {
   const [locations, setLocations] = useState([]),
         [currentLocation, setCurrentLocation] = useState(''),
         [units, setUnits] = useState({ unit: 'metric', deg: 'C', speed: 'm/s' }),
+        [weather, setWeather] = useState(null),
+        [country, setCountry] = useState(null),
+        [forecastDay, setForecastDay] = useState(null),
+        [forecastWeek, setForecastWeek] = useState(null),
         [loading, setLoading] = useState(false),
         [error, setError] = useState(null),
         [warning, setWarning] = useState(null);
@@ -20,7 +25,7 @@ const App = () => {
     setWarning(null);
     setLoading(true);
 
-    fetchPosition(`q=${query}`);
+    fetchPositionData(`q=${query}`);
   };
 
   const addToList = () => {
@@ -35,14 +40,13 @@ const App = () => {
     }
   };
 
-  const getCurrentPos = () => {
-    setLoading(true);
+  const getPosition = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         position => {
           const lon = position.coords.longitude;
           const lat = position.coords.latitude;
-          fetchPosition(`lat=${lat}&lon=${lon}`);
+          fetchPositionData(`lat=${lat}&lon=${lon}`);
           console.log('position updated!');
         },
         error => {
@@ -56,13 +60,12 @@ const App = () => {
       );
     } else {
       alert(
-        'GeoLocation not available, please select your location manually with the search function'
+        'GeoLocation not available'
       );
-      setLoading(false);
     }
   };
 
-  const fetchPosition = query => {
+  const fetchPositionData = query => {
     FETCH(`weather?${query}`)
       .then(data => {
         setCurrentLocation(data);
@@ -72,20 +75,58 @@ const App = () => {
         console.error(`Error fetching data with query: '${error}'`);
         setError(query);
       })
-      .finally(() => {
-        setLoading(false);
-      });
   };
 
+  const fetchWeatherData = (location, units) => {
+    const countryCodes = require('i18n-iso-countries');
+    countryCodes.registerLocale(require('i18n-iso-countries/langs/en.json'));
+
+    (async () => {
+      if (location && units) {
+        await Promise.all([
+          FETCH(`weather?id=${location.id}&units=${units.unit}`)
+          .then(data => {
+            setWeather(data);
+            setCountry(
+              countryCodes.getName(data.sys.country, 'en', {
+                select: 'official'
+              })
+            );
+          }),
+          FETCH(`forecast?id=${location.id}&units=${units.unit}&cnt=8`)
+          .then(data => {
+            setForecastDay(data.list);
+          }),
+          FETCH(
+            `onecall?lat=${location.coord.lat}&lon=${location.coord.lon}&exclude=minutely,hourly&units=${units.unit}`)
+          .then(data => {
+            setForecastWeek(data.daily);
+          })
+        ])
+      }
+    })();
+  }
+
   useEffect(() => {
+    setLoading(true);
     if (localStorage.getItem('list')) {
       setLocations(JSON.parse(localStorage.getItem('list')));
     }
 
     if (!currentLocation) {
-      getCurrentPos();
+      getPosition();
     }
+
+    setLoading(false);
   }, [loading, currentLocation]);
+
+  useEffect(() => {
+    if (currentLocation) {
+      fetchWeatherData(currentLocation, units);
+    }
+  }, [currentLocation, units])
+
+  if (loading || !currentLocation || !units || !weather || !forecastDay || !forecastWeek) return <Loading />
 
   return (
     <>
@@ -97,16 +138,24 @@ const App = () => {
             locations={locations}
             current={currentLocation}
             onSelect={location => setCurrentLocation(location)}
-            clickShowCurrPos={getCurrentPos}
+            clickShowCurrPos={getPosition}
+            weather={weather}
           />
 
           {error && <MessageError messageErr={error} />}
 
+          {!loading && currentLocation && weather && units && forecastDay && forecastWeek && (
           <Main
             location={currentLocation}
             units={units}
             addToList={addToList}
+            setWeather={setWeather}
+            weather={weather}
+            country={country}
+            forecastDay={forecastDay}
+            forecastWeek={forecastWeek}
           />
+          )}
 
           {warning && <MessageWarning messageWarn={warning} />}
 
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -6,111 +6,71 @@ import CurrentWeather from '../current_weather';
 import './Main.scss';
 import Loading from '../loading';
 
-const Main = ({ location, units, addToList }) => {
-  const [weather, setWeather] = useState(null),
-        [forecastDay, setForecastDay] = useState(null),
-        [forecastWeek, setForecastWeek] = useState(null),
-        [loading, setLoading] = useState(false),
-        [country, setCountry] = useState('');
+const Main = ({
+  location,
+  units,
+  addToList,
+  weather,
+  country,
+  forecastWeek,
+  forecastDay
+}) => {
 
-  const countryCodes = require('i18n-iso-countries');
-  countryCodes.registerLocale(require('i18n-iso-countries/langs/en.json'));
-
-  useEffect(() => {
-    setLoading(true);
-
-    (async () => {
-      if (location) {
-        await Promise.all([
-          FETCH(`weather?id=${location.id}&units=${units.unit}`)
-          .then(data => {
-            setWeather(data);
-            setCountry(
-              countryCodes.getName(data.sys.country, 'en', {
-                select: 'official'
-              })
-            );
-          }),
-          FETCH(`forecast?id=${location.id}&units=${units.unit}&cnt=8`)
-          .then(data => {
-            setForecastDay(data.list);
-          }),
-          FETCH(
-            `onecall?lat=${location.coord.lat}&lon=${location.coord.lon}&exclude=minutely,hourly&units=${units.unit}`)
-          .then(data => {
-            setForecastWeek(data.daily);
-          })
-        ])
-        .finally(() => {
-          setLoading(false);
-        });
-      }
-    })();
-  }, [location, units]);
-
-  if (loading || !location || !units || !ForecastDay || !ForecastWeek || !weather) return <Loading />
+  const timeOfDay = (weather) => {
+    return weather.weather[0].icon.includes('d') ? 'day' : 'night';
+  }
 
   return (
     <>
-      {!loading && location && weather && units && (
-        <div id="wrapper" className={`gradient-${weather.weather[0].icon.includes('d') ? 'day' : 'night'}-${weather.weather[0].main.toLowerCase()}`}>
-          <main>
+      <div
+        id="wrapper"
+        className={`gradient-${timeOfDay(weather)}-${weather.weather[0].main.toLowerCase()}`}
+      >
+        <main>
+          <CurrentWeather
+            weather={weather.main}
+            time={weather.dt}
+            details={weather.weather[0]}
+            wind={weather.wind}
+            sun={weather.sys}
+            tod={timeOfDay(weather)}
+            units={units}
+            city={location.name}
+            country={country}
+            clickSaveLocation={addToList}
+          />
 
-              <CurrentWeather
-                weather={weather.main}
-                time={weather.dt}
-                details={weather.weather[0]}
-                wind={weather.wind}
-                sun={weather.sys}
-                tod={weather.weather[0].icon.includes('d') ? 'day' : 'night'}
-                units={units}
-                city={location.name}
-                country={country}
-                clickSaveLocation={addToList}
-              />
+          <div className="day-forecast">
+            <h2>24 hour forecast</h2>
+            <ol>
+              {forecastDay &&
+                forecastDay.map(forecast => (
+                  <li key={forecast.dt}>
+                    <ForecastDay
+                      weather={forecast.main}
+                      time={forecast.dt}
+                      details={forecast.weather[0]}
+                      tod={timeOfDay(forecast)}
+                      units={units}
+                    />
+                  </li>
+                ))}
+            </ol>
+          </div>
 
-            <div className="day-forecast">
-              <h2>24 hour forecast</h2>
-              <ol>
-                {forecastDay && (
-                  forecastDay.map(forecast => (
-                    <li key={forecast.dt}>
-                      <ForecastDay
-                        weather={forecast.main}
-                        time={forecast.dt}
-                        details={forecast.weather[0]}
-                        tod={
-                          forecast.weather[0].icon.includes('d')
-                            ? 'day'
-                            : 'night'
-                        }
-                        units={units}
-                      />
-                    </li>
-                )))}
-              </ol>
-            </div>
-
-            <div className="week-forecast">
-              <h2>7 day forecast</h2>
-              <ol>
-              {forecastWeek && (
+          <div className="week-forecast">
+            <h2>7 day forecast</h2>
+            <ol>
+              {forecastWeek &&
                 forecastWeek.slice(1).map(forecast => (
-                    <li key={forecast.dt}> 
-                      <ForecastWeek
-                        weather={forecast}
-                        units={units}
-                      />
-                    </li>
-                )))}
-              </ol>
-            </div>
-
-          </main>
-        </div>
-      )}
-
-      {loading && <Loading />}
+                  <li key={forecast.dt}>
+                    <ForecastWeek weather={forecast} units={units} />
+                  </li>
+                ))}
+            </ol>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
