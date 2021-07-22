8e04b95cf232df6be1bdde74b7dcd4e1ad5995ec
Karin Stenwall <karin.stenwall@gmail.com>
2021-07-22 20:06:26 +0200

    add: timeout to error & warning messages

--- src/components/message_error/index.jsx
+++ src/components/message_error/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './MessageError.scss';
 
 const MessageError = ({ messageErr }) => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsHidden(true), 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [])
+
   return (
-    <div className="error">
+    <div className={`error ${isHidden ? 'hidden' : null}`}>
       {messageErr}
     </div>
-  )    
+  )  
 };
 
 export default MessageError;
--- src/components/message_warning/index.jsx
+++ src/components/message_warning/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './MessageWarning.scss';
 
 const MessageWarning = ({ messageWarn }) => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsHidden(true), 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [])
+
   return (
-    <div className="warning">
+    <div className={`warning ${isHidden ? 'hidden' : null}`}>
       {messageWarn}
     </div>
-  )    
+  )
 };
 
 export default MessageWarning;
